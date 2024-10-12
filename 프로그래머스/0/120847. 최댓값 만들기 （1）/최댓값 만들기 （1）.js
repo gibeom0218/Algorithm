9ac76961a41420c49dec96754d2dbb347538b8ec
gibeom0218 <108421517+gibeom0218@users.noreply.github.com>
2024-10-13 00:21:12 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = 0;
+    
+    numbers.sort((a,b)=>{
+        return a-b;
+    })
+    
+    answer=numbers[numbers.length-1]*numbers[numbers.length-2]
+    
+    
+    return answer;
+}
