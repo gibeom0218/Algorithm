993432293882833178ff153778ab01c8113552e2
gibeom0218 <108421517+gibeom0218@users.noreply.github.com>
2024-10-17 22:35:23 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.68 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+    var answer = 1;
+    
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr[0].length;j++){
+            if(arr[i][j]!==arr[j][i]){
+                answer=0;
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
